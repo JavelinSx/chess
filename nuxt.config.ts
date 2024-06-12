237afff25dc6408f25caaf03fe3614c33f296112
Nikita <74298153+JavelinSx@users.noreply.github.com>
2024-06-12 11:46:28 +0300

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@unocss/nuxt'],
+  css: ['@/app/global.css', '@unocss/reset/tailwind.css'],
+
+  imports: {
+    dirs: [
+      'shared/*',
+      // Auto-import all files from shared directory. You can specify only need paths | https://nuxt.com/docs/guide/directory-structure/composables
+    ],
+  },
+
+  dir: {
+    pages: 'app/routes',
+  },
+
+  components: [
+    {
+      path: '~/shared',
+      pathPrefix: false,
+      // Auto-import all components from shared directory.
+    },
+  ],
+
+})
