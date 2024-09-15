4a7717d0ff266bf1746626faaa5fe7081824ba6c
JavelinSx <zuev499@gmail.com>
2024-09-15 19:47:12 +0300

    test deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,9 +24,9 @@ export default defineNuxtConfig({
   },
 
   runtimeConfig: {
-    apiBase: '/api',
-    mongodbUri: 'mongodb://localhost:27017/chess_game',
-    jwtSecret: 'your_secret_key_here',
+    apiBase: '/api' || process.env.API_BASE,
+    mongodbUri: 'mongodb://localhost:27017/chess_game' || process.env.MONGODB_URI,
+    jwtSecret: 'your_secret_key_here' || process.env.JWT_SECRET,
   },
 
   ssr: false,
